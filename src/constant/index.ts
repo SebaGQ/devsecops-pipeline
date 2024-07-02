1f3f283c3d69fa4581a4c590204b3b91af76fb33
SebaGQ <segutierrez.q@gmail.com>
2024-07-02 16:32:28 -0400

    test: se añade un key hardcodeado

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,6 +2,7 @@ import { CustomGenre } from "src/types/Genre";
 
 export const API_ENDPOINT_URL = import.meta.env.VITE_APP_API_ENDPOINT_URL;
 export const TMDB_V3_API_KEY = import.meta.env.VITE_APP_TMDB_V3_API_KEY;
+export const API_KEY = "12345-ABCD";
 
 export const MAIN_PATH = {
   root: "",
